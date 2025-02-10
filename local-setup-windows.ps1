bf4d3fae0090e3d967e76fa8fc7c6d861367a8d4
Liz Gibson <gibson.lizzi@gmail.com>
2025-02-10 18:48:09 -0500

    add git install for windows

--- local-setup-windows.ps1
+++ local-setup-windows.ps1
@@ -1,9 +1,9 @@
 # Chocolately should be installed
 if (get-command choco) {
-    echo "chocolatey already installed"
+  echo "chocolatey already installed"
 } else {
-    echo "installing chocolatey"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
+  echo "installing chocolatey"
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 # Nano should be installed
@@ -12,4 +12,12 @@ if (get-command nano) {
 } else {
   echo "installing nano"
   choco install nano
+}
+
+# Git should be installed
+if (get-command git) {
+  echo "git is already installed"
+} else {
+  echo "installing git"
+  choco install git
 }
