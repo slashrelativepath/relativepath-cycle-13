2b8118492baefca53ca4dfeb3dde9dbc084753ba
William McQuaid <whmcquaid@gmail.com>
2025-02-10 15:44:42 -0800

    Add nano install.

--- local-setup-windows.ps1
+++ local-setup-windows.ps1
@@ -1,6 +1,15 @@
+# Chocolately should be installed
 if (get-command choco) {
     echo "chocolatey already installed"
 } else {
     echo "installing chocolatey"
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
+}
+
+# Nano should be installed
+if (get-command nano) {
+  echo "nano already installed"
+} else {
+  echo "installing nano"
+  choco install nano
 }
