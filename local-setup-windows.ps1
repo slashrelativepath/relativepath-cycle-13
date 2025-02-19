794c095d9dc8fb6e5412da8ecfcedb296bbd5317
William McQuaid <whmcquaid@gmail.com>
2025-02-19 15:53:21 -0800

    Add virtualbox and multipass install.

--- local-setup-windows.ps1
+++ local-setup-windows.ps1
@@ -21,3 +21,19 @@ if (get-command git) {
   echo "installing git"
   choco install git
 }
+
+# Virtualbox should be installed
+if (get-command virtualbox) {
+  echo "virtualbox is already installed"
+} else {
+  echo "installing virtualbox"
+  choco install virtualbox
+}
+
+# Multipass should be installed
+if (get-command multipass) {
+  echo "multipass is already installed"
+} else {
+  echo "installing multipass"
+  choco install multipass --params="'/HyperVisor:VirtualBox'"
+}
