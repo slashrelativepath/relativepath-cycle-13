a8eab34d8c2e339c50686ddad5eb72baa788eef2
Devin Beu <devinbeu@gmail.com>
2025-02-20 15:34:41 -0800

    here we go with the darwin mulitpass@

--- local-setup-darwin.sh
+++ local-setup-darwin.sh
@@ -8,6 +8,7 @@ else
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# nano should be installed
 if (which nano)
 then
   echo "nano already installed" 
@@ -16,12 +17,22 @@ else
   brew install nano
 fi
 
+# git should be installed
 if (git --version)
 then 
   echo "git already installed"
 else
   echo "installing git"
   brew install git
+fi
+
+# mulitpass should be installed
+if (multipass --version)
+then
+  echo "multipass is already installed"
+else 
+  echo "installing multipass"
+  brew install multipass
 fi
 
 
